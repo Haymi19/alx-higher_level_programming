cfa0848cee27c47076750f965e6bb2b293cff178
Haimanot Eshete <haimanoteshete685@gmail.com>
2023-01-25 08:12:12 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class Square that defines a square by size"""
+
+
+class Square:
+    """ class Square"""
+    def __init__(self, size):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
