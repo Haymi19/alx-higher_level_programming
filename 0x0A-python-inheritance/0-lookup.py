a7e03f824da55e8d54e4c2c0fbb470000cbe0faa
Haimanot Eshete <haimanoteshete685@gmail.com>
2023-02-06 08:46:19 -0800

    1-my_list.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def lookup(obj):
-
-
-    """ Function that returns the list of available attributes
+    """ Returns the list of available attributes
         and methods of an object
     Args:
         obj: instance of the class
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class MyList(list):
+    """ Inherits the attributes references of class list
+    Args:
+        list: class list
+    """
+
+    def print_sorted(self):
+        """ Prints the sorted list """
+        l_sorted = self.copy()
+        l_sorted.sort()
+        print(l_sorted)
