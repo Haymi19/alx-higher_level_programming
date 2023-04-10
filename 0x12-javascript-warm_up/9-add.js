df2a58f4319023504258db078f66d6ff39db3465
Haimanot Eshete <haimanoteshete685@gmail.com>
2023-04-10 11:51:09 -0700

    update

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,8 +3,7 @@
 // Script that prints the addition of 2 integers.
 
 function add(a, b) {
-	let add = a + b;
-	return add;
+	return a +b;
 }
 
 console.log(add(Number(process.argv[2]), Number(process.argv[3])));
