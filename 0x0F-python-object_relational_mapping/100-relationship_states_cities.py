a95f090b25464e269eb57e1574390ed9c73a44c7
Haimanot Eshete <haimanoteshete685@gmail.com>
2023-04-16 07:16:01 -0700

    relationship state

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-This script prints all City objects from
-the database `hbtn_0e_14_usa`.
+This script prints all City objects from the
+database `hbtn_0e_14_usa`.
 """
 
 from sys import argv
-from model_state import Base, State
-from model_city import City
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -18,14 +18,14 @@ if __name__ == "__main__":
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
     engine = create_engine(db_uri)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
 
     session = Session()
+    cal_state = State(name='California')
+    sfr_city = City(name='San Francisco')
+    cal_state.cities.append(sfr_city)
 
-    query = session.query(City, State).join(State)
-
-    for _c, _s in query.all():
-        print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
-
+    session.add(cal_state)
     session.commit()
     session.close()
